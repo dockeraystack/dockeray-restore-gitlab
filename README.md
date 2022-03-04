b82c70b98b16d9da2452e04025923034a2c5afc8
Thiago Moreira <tmoreira2020@gmail.com>
2022-03-03 23:59:31 -0300

    Add project setup

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # dockeray-restore-gitlab
+
+Docker Compose usage
+
+```
+  rst:
+    container_name: rst.mydomain.com
+    environment:
+      - BACKUP_GITLAB_API_ENDPOINT=https://gitlab.com/api/v4/projects/myprojectid/packages/generic
+      - BACKUP_GITLAB_ACCESS_TOKEN=mygitlabtoken
+      - LATEST=${LATEST}
+      - ENVIRONMENT=${ENVIRONMENT}
+    image: dockeraystack/dockeray-restore-gitlab:10.7.3-1.1.0
+    hostname: rst
+    domainname: mydomain.com
+    volumes:
+      - restore:/data:rw
+```
---
+++ get.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+#
+# Copyright © 2022 Thiago Moreira (tmoreira2020@gmail.com)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+set -e
+
+echo $(date "+%Y/%m/%d %H:%M:%S")" Job get started"
+
+umask 0
+
+set -e
+
+: ${DATA_PATH:="/data"}
+: ${BACKUP_GITLAB_API_ENDPOINT:?"BACKUP_GITLAB_API_ENDPOINT env variable is required"}
+: ${BACKUP_GITLAB_ACCESS_TOKEN:?"BACKUP_GITLAB_ACCESS_TOKEN env variable is required"}
+
+LATEST=${LATEST}
+
+if [[ -z "$LATEST" ]]; then
+    echo $(date "+%Y/%m/%d %H:%M:%S")" Retrieving date of the latest backup."
+
+    is_there_backup=$(curl -L --header "PRIVATE-TOKEN: $BACKUP_GITLAB_ACCESS_TOKEN" $BACKUP_GITLAB_API_ENDPOINT/liferay-backup/$ENVIRONMENT/LATEST -o /tmp/LATEST -w '%{http_code}\n' -s)
+
+    if [[ $is_there_backup -eq 200 ]]; then
+        latest=$(head -n 1 /tmp/LATEST)
+    else
+        echo $(date "+%Y/%m/%d %H:%M:%S")" [WARN] No backup was found"
+    fi
+else
+    latest=$LATEST
+fi
+
+echo $(date "+%Y/%m/%d %H:%M:%S")" Looking for backups at date: $latest"
+
+dump_file_path=$(head -n 2 /tmp/LATEST | tail -n 1)
+
+is_there_dump_file=$(curl -L --header "PRIVATE-TOKEN: $BACKUP_GITLAB_ACCESS_TOKEN" $dump_file_path -o /tmp/dump.sql.gz -w '%{http_code}\n' -s)
+
+if [[ $is_there_dump_file -eq 200 ]]; then
+    cp /tmp/dump.sql.gz $DATA_PATH/dump.sql.gz
+else
+    echo $(date "+%Y/%m/%d %H:%M:%S")" [WARN] No backup dump file at path $dump_file_path was found"
+fi
+
+data_file_path=$(head -n 3 /tmp/LATEST | tail -n 1)
+
+is_there_data_file=$(curl -L --header "PRIVATE-TOKEN: $BACKUP_GITLAB_ACCESS_TOKEN" $data_file_path -o /tmp/data.tgz -w '%{http_code}\n' -s)
+
+if [[ $is_there_data_file -eq 200 ]]; then
+    cp /tmp/data.tgz $DATA_PATH/data.tgz
+else
+    echo $(date "+%Y/%m/%d %H:%M:%S")" [WARN] No backup data file at path $data_file_path was found"
+fi
+
+echo $(date "+%Y/%m/%d %H:%M:%S")" Job get finished: $(date)"
